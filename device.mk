d06a051265a114fd4edcbfad569fde368de0468a
Kieran Elmes <kelmes@cs.otago.ac.nz>
2020-04-17 14:43:41 +1200

    Revert "device: Add usensord override with custom kb vibration duration"
    This reverts commit b830fda371658f2a3f34646bedf508cddade81da.

--- device.mk
+++ device.mk
@@ -74,8 +74,7 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/rootdir/system/halium/etc/pulse/touch.pa:system/halium/etc/pulse/touch.pa \
     $(DEVICE_PATH)/rootdir/system/halium/usr/share/bluetooth-touch/loire:system/halium/usr/share/bluetooth-touch/loire \
     $(DEVICE_PATH)/rootdir/system/halium/usr/share/apparmor/hardware/graphics.d/apparmor-easyprof-ubuntu_android:system/halium/usr/share/apparmor/hardware/graphics.d/apparmor-easyprof-ubuntu_android \
-    $(DEVICE_PATH)/rootdir/system/halium/usr/share/apparmor/hardware/video.d/apparmor-easyprof-ubuntu_android:system/halium/usr/share/apparmor/hardware/video.d/apparmor-easyprof-ubuntu_android \
-    $(DEVICE_PATH)/rootdir/system/halium/usr/share/upstart/sessions/usensord.conf:system/halium/usr/share/upstart/sessions/usensord.conf
+    $(DEVICE_PATH)/rootdir/system/halium/usr/share/apparmor/hardware/video.d/apparmor-easyprof-ubuntu_android:system/halium/usr/share/apparmor/hardware/video.d/apparmor-easyprof-ubuntu_android
 
 PRODUCT_PACKAGES += \
     libubuntu_application_api \
